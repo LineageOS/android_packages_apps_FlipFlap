15a1d9fd2cec91aea8c0d9df42979d2ed62ae652
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-16 06:44:53 -0500

    FlipFlap: Initial iceview case
    Change-Id: I5f33833414c8135e8ac6d6350c288262293b4c77

--- src/org/lineageos/flipflap/FlipFlapUtils.java
+++ src/org/lineageos/flipflap/FlipFlapUtils.java
@@ -36,6 +36,7 @@ public class FlipFlapUtils {
     static final int COVER_STYLE_DOTCASE = 1;
     static final int COVER_STYLE_CIRCLE = 2;
     static final int COVER_STYLE_RECTANGULAR = 3;
+    static final int COVER_STYLE_ICEVIEW = 4;
 
     static final int DELAYED_SCREEN_OFF_MS = 5000;
 }
