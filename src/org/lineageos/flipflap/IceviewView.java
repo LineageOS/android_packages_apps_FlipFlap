2cf0552374f102f9d5a387e4a7857e99be6c8f1b
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-16 06:44:53 -0500

    FlipFlap: Initial iceview case
    Change-Id: I5f33833414c8135e8ac6d6350c288262293b4c77

---
+++ src/org/lineageos/flipflap/IceviewView.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2017 The LineageOS Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Also add information on how to contact you by electronic and paper mail.
+ *
+ */
+
+package org.lineageos.flipflap;
+
+import android.app.Notification;
+import android.content.Context;
+import android.service.notification.StatusBarNotification;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import android.util.Log;
+
+import java.util.List;
+
+public class IceviewView extends FlipFlapView {
+    private static final String TAG = "IceviewView";
+
+    private ClockPanel mClockPanel;
+    private final NotificationsAdapter mNotificationsAdapter;
+
+    public IceviewView(Context context) {
+        super(context);
+
+        inflate(mContext, R.layout.iceview_view, this);
+
+        mClockPanel = (ClockPanel) findViewById(R.id.clock_panel);
+        mClockPanel.bringToFront();
+
+        mNotificationsAdapter = new NotificationsAdapter(context);
+        ListView notificationsList = (ListView) findViewById(R.id.iceview_notifications);
+        notificationsList.setAdapter(mNotificationsAdapter);
+    }
+
+    @Override
+    public boolean supportsNotifications() {
+        return true;
+    }
+
+    @Override
+    public void updateNotifications(List<StatusBarNotification> notifications) {
+        mNotificationsAdapter.setNotifyOnChange(false);
+        mNotificationsAdapter.clear();
+        mNotificationsAdapter.addAll(notifications);
+        mNotificationsAdapter.notifyDataSetChanged();
+    }
+
+    private static class NotificationsAdapter extends ArrayAdapter<StatusBarNotification> {
+        public NotificationsAdapter(Context context) {
+            super(context, 0);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflate(getContext(), R.layout.notification_item, parent);
+            }
+
+            Notification notification = getItem(position).getNotification();
+            TextView title = (TextView) convertView.findViewById(R.id.notification_title);
+            title.setText(notification.extras.getString(Notification.EXTRA_TITLE));
+
+            return convertView;
+        }
+    }
+}
