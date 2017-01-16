bf5fe6b81a2dc65812177270667fa01d29a6f9d5
Michael W <baddaemon87@gmail.com>
2017-01-16 09:31:22 +0100

    FlipFlap: Add constants
    Add and use a few constants instead of magic numbers
    
    Change-Id: Ifaa0cead9f456002894847ab6c08cd1d25133119

--- src/org/lineageos/flipflap/FlipFlapUtils.java
+++ src/org/lineageos/flipflap/FlipFlapUtils.java
@@ -32,6 +32,14 @@ public class FlipFlapUtils {
     static final String ACTION_ALARM_DISMISS = "com.android.deskclock.ALARM_DISMISS";
     static final String ACTION_ALARM_SNOOZE = "com.android.deskclock.ALARM_SNOOZE";
 
+    static final int COVER_STATE_OPENED = 0;
+    static final int COVER_STATE_CLOSED = 1;
+
+    // These have to match with "config_deviceCoverType" from res/values/config.xml
+    static final int COVER_STYLE_NONE = 0;
+    static final int COVER_STYLE_DOTCASE = 1;
+    static final int COVER_STYLE_CIRCLE = 2;
+
     public static int getScreenWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
     }
