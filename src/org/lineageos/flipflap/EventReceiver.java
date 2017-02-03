9d8118b5681216158fe57efabab38f81300218ce
Michael W <baddaemon87@gmail.com>
2017-02-03 22:29:27 +0100

    FlipFlap: Allow deactivation
    * Show settings only when cover was closed at least once
    * Introduce "immediately" option for the timeouts
    
    Change-Id: Ica5555cd5638043d6ed035451d4083433c3772c4

--- src/org/lineageos/flipflap/EventReceiver.java
+++ src/org/lineageos/flipflap/EventReceiver.java
@@ -21,8 +21,11 @@
 package org.lineageos.flipflap;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.BatteryManager;
 import android.util.Log;
 import android.view.WindowManagerPolicy.WindowManagerFuncs;
 
@@ -35,12 +38,22 @@ public class EventReceiver extends BroadcastReceiver {
             int lidState = intent.getIntExtra(cyanogenmod.content.Intent.EXTRA_LID_STATE, -1);
             Log.d(TAG, "Got lid state change event, new state " + lidState);
 
+            BatteryManager batMan = (BatteryManager) context.getSystemService(
+                    Context.BATTERY_SERVICE);
+            int timeout = FlipFlapUtils.getTimeout(context, batMan.isCharging());
             Intent serviceIntent = new Intent(context, FlipFlapService.class);
-            if (lidState == WindowManagerFuncs.LID_CLOSED) {
+            if (lidState == WindowManagerFuncs.LID_CLOSED && timeout != 0) {
                 context.startService(serviceIntent);
+                activateSettings(context);
             } else {
                 context.stopService(serviceIntent);
             }
         }
+    }
+
+    private void activateSettings(Context context) {
+        ComponentName settings = new ComponentName(context, FlipFlapSettingsActivity.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(settings, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, 0);
     }
 }
