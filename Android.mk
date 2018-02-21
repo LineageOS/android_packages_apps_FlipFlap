af2931258d062eb19f00f2f5d565d24a8194bcca
Anton Hansson <hansson@google.com>
2018-02-21 14:11:44 +0000

    FlipFlap: Set LOCAL_SDK_VERSION to current
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Bug: 73535841
    Change-Id: Iabb0556dc1c80c7fc7f6c76d61d5e441b03cdce0

--- Android.mk
+++ Android.mk
@@ -18,6 +18,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := FlipFlap
+LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
