74b28b75d6784242c65b695aa1c944abdccf69cf
Florin9doi <florin9doi@gmail.com>
2018-09-16 13:00:44 +0300

    Merge ignoreCall and endCall buttons
    -reuse a single TextView for both buttons: ignoreCall and endCall
    -endCall was completely misplaced, this patch resolves the issue
    -move the image and text for each button in a new View and move
     onClickListener on the parent. This change allows to accept/ignore
     a phonecall by clicking on the text.
    
    Change-Id: I991a1eba3a3652ad0da933b2ee45d2197ef273fb

--- src/org/lineageos/flipflap/CircleView.java
+++ src/org/lineageos/flipflap/CircleView.java
@@ -25,6 +25,7 @@ import android.content.Context;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CircleView extends FlipFlapView {
@@ -37,9 +38,9 @@ public class CircleView extends FlipFlapView {
     private PhonePanel mPhonePanel;
     private ImageButton mAlarmSnoozeButton;
     private ImageButton mAlarmDismissButton;
-    private ImageButton mAnswerCallButton;
-    private ImageButton mIgnoreCallButton;
-    private ImageButton mEndCallButton;
+    private RelativeLayout mAnswerCallButton;
+    private RelativeLayout mIgnoreCallButton;
+    private TextView mEndCallText;
     private TextView mIncomingCallName;
     private TextView mIncomingCallNumber;
     private boolean mRinging;
@@ -60,9 +61,9 @@ public class CircleView extends FlipFlapView {
 
         mIncomingCallName = (TextView) findViewById(R.id.incoming_call_name);
         mIncomingCallNumber = (TextView) findViewById(R.id.incoming_call_number);
-        mAnswerCallButton = (ImageButton) findViewById(R.id.answer_button);
-        mIgnoreCallButton = (ImageButton) findViewById(R.id.ignore_button);
-        mEndCallButton = (ImageButton) findViewById(R.id.end_call_button);
+        mAnswerCallButton = (RelativeLayout) findViewById(R.id.answer_button);
+        mIgnoreCallButton = (RelativeLayout) findViewById(R.id.ignore_button);
+        mEndCallText = (TextView) findViewById(R.id.ignore_text);
         mAnswerCallButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,12 +71,6 @@ public class CircleView extends FlipFlapView {
             }
         });
         mIgnoreCallButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                endCall();
-            }
-        });
-        mEndCallButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 endCall();
@@ -136,12 +131,10 @@ public class CircleView extends FlipFlapView {
             mNextAlarmPanel.setVisibility(View.GONE);
             if (mRinging) {
                 mAnswerCallButton.setVisibility(View.VISIBLE);
-                mIgnoreCallButton.setVisibility(View.VISIBLE);
-                mEndCallButton.setVisibility(View.GONE);
+                mEndCallText.setText(getResources().getString(R.string.ignore_call));
             } else {
                 mAnswerCallButton.setVisibility(View.GONE);
-                mIgnoreCallButton.setVisibility(View.GONE);
-                mEndCallButton.setVisibility(View.VISIBLE);
+                mEndCallText.setText(getResources().getString(R.string.end_call));
             }
         } else if (mAlarmActive) {
             mDatePanel.setVisibility(View.VISIBLE);
